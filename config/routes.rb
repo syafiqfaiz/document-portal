f1c9faa6ae099fffdeffd3dff3874a2ddb24eec4
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2016-02-16 01:17:25 +0800

    removing documents resources in routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,12 +22,8 @@ Rails.application.routes.draw do
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
 
-  root 'clearance/sessions#new'
 
   resources :users, only: [:show]
-
-  # test upload
-  resources :documents, only: [:index, :new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
