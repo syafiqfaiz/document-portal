5356b065796373f73083da8d03048d63af3bae19
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2016-02-16 23:20:39 +0800

    user can view documents and add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
 
-  resources :main, only: [:index]
+  resources :home, only: [:index]
+  resources :documents, only: [:index, :show]
+  # documents_path  GET /documents(.:format)  documents#index
+  # document_path GET /documents/:id(.:format)  documents#show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
