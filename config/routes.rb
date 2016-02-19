45aa6f180e0eedd58e719dff4dbe453247b0bc8f
ahmadsyahirmuhamad <ahmadsyahirmuhamad@gmail.com>
2016-02-19 19:55:33 +0800

    tweak main page, add plan list to menu

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :payment_callbacks, only: :create
 
-  resources :subscriptions, only: [:update, :create]
+  resources :subscriptions, only: [:index, :update, :create]
 
   resources :plans, only: :index
 
@@ -39,9 +39,9 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   resources :home, only: [:index]
-  
+
   root 'home#index'
-  
+
   # documents_path  GET /documents(.:format)  documents#index
   # document_path GET /documents/:id(.:format)  documents#show
 
